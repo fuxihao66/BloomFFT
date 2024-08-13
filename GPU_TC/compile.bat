dcf2dcd23bedc9b4390a864ab1f5150442955569
Xihao Fu <fuxihao66@gmail.com>
2024-08-13 20:31:01 +0800

    add define macro

--- GPU_TC/compile.bat
+++ GPU_TC/compile.bat
@@ -1,1 +1,3 @@
 .\dxc_preview\bin\x64\dxc.exe /D_WAVE_OP /Zi /E"CSMain" /Vn"g_TwoForOneShader_CS" /HV"2018" /enable-16bit-types /Tcs_6_8 /Fh"FFT_TwoForOne.hlsl.h" /nologo "./FFT_TwoForOne.hlsl"
+
+@REM https://strontic.github.io/xcyclopedia/library/dxc.exe-0C1709D4E1787E3EB3E6A35C85714824.html
